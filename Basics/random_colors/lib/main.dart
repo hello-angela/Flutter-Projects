2b2ebfc57ab2d16c70c04354e3c0cbbe3f024e74
Angela Martinescu <angela.pelobello@mmltd.co.za>
2022-11-19 18:13:01 +0200

    where is my button??

--- Basics/random_colors/lib/main.dart
+++ Basics/random_colors/lib/main.dart
@@ -14,22 +14,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: const Scaffold(
-        body: BackgroundColor(),
-      ),
+      home: const Scaffold(body: ColorChanger()),
     );
   }
 }
 
-class BackgroundColor extends StatelessWidget {
+class BackgroundColor extends StatefulWidget {
   const BackgroundColor({super.key});
+
+  @override
+  State<BackgroundColor> createState() => _BackgroundColor();
+}
+
+class _BackgroundColor extends State<BackgroundColor> {
+  randomColor() {
+    return Color(math.Random().nextInt(0xffffffff));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(color: randomColor());
   }
+}
 
-  randomColor() {
-    return Color(math.Random().nextInt(0xffffffff));
+class ColorChanger extends StatefulWidget {
+  const ColorChanger({super.key});
+
+  @override
+  State<ColorChanger> createState() => _ColorChanger();
+}
+
+class _ColorChanger extends State<ColorChanger> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: ElevatedButton(
+        style: style,
+        onPressed: () => {const ColorChanger()},
+        child: const Text('CHANGE'),
+      ),
+    );
   }
 }
